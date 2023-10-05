87daac0d6e645368a1331a83202a9531ddb1c928
Andi <alexandra.e.burke@gmail.com>
2023-10-05 13:37:45 -0400

    PayrollUploadOne

---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner payrollCalculator =
+        String yourName = "What is your name?";
+
+
+    }
+}
